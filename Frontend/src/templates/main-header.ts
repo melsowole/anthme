6ee668313f855e423da265ea5f29aa937d28996a
melsowole <82158719+melsowole@users.noreply.github.com>
2024-03-25 14:11:40 +0100

    bse

--- Frontend/src/templates/main-header.ts
+++ Frontend/src/templates/main-header.ts
@@ -1,3 +1,32 @@
-const header = `<header class="main-header"></header>`;
+const header = `
+<header class="main-header">
+    <a class="logo" href="/">ANTHME</a>
+    <nav class="user-nav">
+        <a class="unstyle pill" href="/create-post" aria-label="Create Post" aria-hidden="true">
+            <span class="icon material-symbols-outlined">add</span>
+            Create
+        </a>
+        <div class="user">
+            <div class="img-icon"></div>
+        </div>
+    </nav>
+</header>
+`;
+
+const profileMenu = `
+<div class="profile-menu">
+    <div class="user">
+        <div class="img-icon"></div>
+        <a href="/profile/__username__">View Profile</a>
+        <small class="username">u/__username__</small>
+    </div>
+    <ul>
+        <li>Dark mode toggle</li>
+        <li>Log Out</li>
+    </ul>
+    <hr>
+    <a>Delete Account</a>
+</div>
+`;
 
 export { header };
